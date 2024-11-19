3770f37ce3a2ba8c1c369926ed809e4d0777707a
PhuonggBuii <namphuongcbvn267@gmail.com>
2024-11-19 23:53:22 +0700

    demo

--- expense-management-system/src/main/java/com/example/expense_management_system/security/WebSecurityConfig.java
+++ expense-management-system/src/main/java/com/example/expense_management_system/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-
+/* 
 package com.example.expense_management_system.security;
 
 import org.springframework.context.annotation.Bean;
@@ -25,6 +25,23 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+*/
+
+package com.example.expense_management_system.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
